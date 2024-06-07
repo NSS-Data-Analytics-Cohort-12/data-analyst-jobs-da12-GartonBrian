a262e074616bccfbd493bdb914ddb0b4ab6d5fb8
GartonBrian <Brian.Thomas.Garton@gmail.com>
2024-06-06 21:17:37 -0500

    dataanalystcomplete

--- scripts/data-analyst-jobs-da12.sql
+++ scripts/data-analyst-jobs-da12.sql
@@ -10,7 +10,8 @@
 -- COUNT(*)
 -- FROM data_analyst_jobs;
 
--- 2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
+-- 2.	Write a query to look at just the first 10 rows. What company 
+-- is associated with the job posting on the 10th row?
 
 -- SELECT
 -- company
@@ -19,7 +20,8 @@
 
 --ExxonMobil
 
--- 3.	How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+-- 3.	How many postings are in Tennessee? How many are there in either
+-- Tennessee or Kentucky?
 
 -- SELECT 
 -- count(location) AS postings_In_Tennessee
@@ -41,18 +43,20 @@
 -- FROM data_analyst_jobs
 -- WHERE star_rating >=5 AND location = 'TN'
 
---1
+--1 
+
 -- 5.	How many postings in the dataset have a review count between 500 and 1000?
 -- SELECT
--- 	COUNT (review_count) AS half_to_one_k_rewviews
+-- 	COUNT (review_count) AS tween_one_n_half_k
 -- FROM data_analyst_jobs
 -- WHERE review_count 
 -- 	BETWEEN 500 AND 1000;
 
 -- 151
 
--- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
-
+-- 6.	Show the average star rating for companies in each state. 
+-- The output should show the state as `state` and the average rating
+-- 	for the state as `avg_rating`. Which state shows the highest average rating?
 
 -- SELECT 
 -- 	avg(star_rating) as avg_rating,
@@ -63,18 +67,40 @@
 -- group by company, location, star_rating
 -- order by star_rating desc;
 
+-- There is a tie between the top twelve companies 
+-- "Globoforce Limited"
+-- "ClearView Healthcare Partners"
+-- "Alteryx, Inc."
+-- "Association of Universities for Research in Astronomy"
+-- "Doximity"
+-- "GoodRx"
+-- "Bowery Farming"
+-- "Vista Technology Services"
+-- "Acquisition Life Cycle Management"
+-- "Beaver Dam Community Hospitals, Inc."
+-- "Ramsey Solutions"
+-- "Blade"
+
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
--- SELECT count(DISTINCT(title))
+
+-- SELECT 
+-- count(DISTINCT(title))
 -- FROM data_analyst_jobs;
 
 -- 881
+
 -- 8.	How many unique job titles are there for California companies?
--- SELECT count(DISTINCT(title))
+-- SELECT 
+-- count(DISTINCT(title))
 -- FROM data_analyst_jobs
 -- WHERE location = 'CA';
 
 -- 230
--- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
+
+ -- 9.	Find the name of each company and its average star rating for all 
+-- companies that have more than 5000 reviews across all locations. How many 
+-- 	companies are there with more that 5000 reviews across all locations?
+
 -- SELECT 
 -- 	avg(star_rating) as avg_rating,
 -- 	company as company,
@@ -86,7 +112,9 @@
 
 -- 83
 
--- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+-- 10.	Add the code to order the query in #9 from highest to lowest 
+-- average star rating. Which company with more than 5000 reviews across
+-- 	all locations in the dataset has the highest star rating? What is that rating?
 
 -- SELECT 
 -- 	avg(star_rating) as avg_rating,
@@ -99,15 +127,18 @@
 -- ORDER BY avg_rating desc;
 --Looks like theres a tie between the top 9 companies
 
--- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+-- 11.	Find all the job titles that contain the word ‘Analyst’. 
+-- How many different job titles are there? 
 
--- SELECT COUNT(DISTINCT title)
+-- SELECT 
+-- COUNT(DISTINCT title)
 -- FROM data_analyst_jobs
 -- WHERE title LIKE '%analyst%' or title like '%Analyst%'; 
 
 -- 757
 
--- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+-- 12.	How many different job titles do not contain either the word ‘Analyst’ or
+-- the word ‘Analytics’? What word do these positions have in common?
 
 -- SELECT	
 -- 	COUNT(DISTINCT title)
@@ -115,10 +146,28 @@
 -- WHERE title NOT LIKE 'Analyst' AND title NOT LIKE 'Analytics';
 
 -- 881
---ANALY
+--"ANALY"
 
 -- **BONUS:**
--- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
+-- You want to understand which jobs requiring SQL are hard to fill.
+-- Find the number of jobs by industry (domain) that require SQL and
+-- have been posted longer than 3 weeks. 
 --  - Disregard any postings where the domain is NULL. 
---  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
---   - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
+--  - Order your results so that the domain with the greatest number 
+-- of `hard to fill` jobs is at the top. 
+--  - Which three industries are in the top 3 on this list? How many
+-- jobs have been listed for more than 3 weeks for each of the top 3?
+
+-- SELECT 
+-- 	count(title) AS number_of_hard_to_fill_jobs,
+-- 	domain AS field,
+-- 	skill
+-- FROM data_analyst_jobs
+-- where 
+-- 	days_since_posting > 21
+-- 	AND SKILL LIKE '%SQL%'
+-- 	AND domain IS NOT NULL
+-- GROUP BY domain, days_since_posting, skill
+-- ORDER BY number_of_hard_to_fill_jobs DESC 
+-- LIMIT 3;
+
